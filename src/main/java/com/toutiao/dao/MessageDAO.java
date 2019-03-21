1806543ed701df3bc691c0ef69efe9d9e51c1346
hellooying <15642330153@163.com>
2019-03-21 15:35:58 +0800

    学习多线程

--- src/main/java/com/toutiao/dao/MessageDAO.java
+++ src/main/java/com/toutiao/dao/MessageDAO.java
@@ -28,6 +28,6 @@ public interface MessageDAO {
     @Select({"select ", INSERT_FIELDS, " ,count(id) as id from ( select * from ", TABLE_NAME, " where from_id=#{userId} or to_id=#{userId} order by id desc) tt group by conversation_id  order by created_date desc limit #{offset}, #{limit}"})
     List<Message> getConversationList(@Param("userId") int userId,
                                       @Param("offset") int offset, @Param("limit") int limit);
-    @Update({"update ",TABLE_NAME," set has_read=1 where to_id=#{userId} and conversation_id=#{conversationId}"})
+    @Update({"update ",TABLE_NAME," set has_read=1 where to_id=#{userId} and conversation_id=#{conversationId} and has_read=0"})
     int updateUnreadCount(@Param("userId") int userId, @Param("conversationId") String conversationId);
 }
